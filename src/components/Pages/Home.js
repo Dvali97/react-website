b326ab8d146e5ade4c60e24e6addec0092747ce6
Dvali97 <ditodvali1997@gmail.com>
2022-12-11 06:01:14 -0800

    Updated

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,17 @@
+import '../../App.css'
+import Cards from '../CardItem/Cards';
+import HeroSection from './../HeroSection/HeroSection';
+import Footer from './Footer/Footer';
+
+function Home() {
+    return (
+        <>
+        <HeroSection />
+        <Cards />
+        <Footer />
+        </>
+    )
+
+}
+
+export default Home;
---
+++ src/components/Pages/Services.js
@@ -0,0 +1,8 @@
+import '../../App.css';
+
+let Services = () => {
+    return <h1 className='services'>SERVICES</h1>
+}
+
+export default Services;
+
